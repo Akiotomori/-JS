72c5db0ffb277d8c23e1fc23dbfbcbedbc347809
Akiotomori <r.korotkevich@internet.ru>
2025-04-20 00:11:31 +0300

    +1.1

--- lesson_1.js
+++ lesson_1.js
@@ -4,3 +4,11 @@ let name, admin
 name = 'Jonn'
 admin = name
 alert(admin)
+
+const planetEarth = 'Моя планета'
+const currentVisitor = 'Текущий посетитель'
+
+let nameIlya = "Ilya";
+alert( `hello ${1}` ); // выведет hello 1
+alert( `hello ${"name"}` ); // выведет hello name
+alert( `hello ${nameIlya}` ); // выведет hello Jonn
